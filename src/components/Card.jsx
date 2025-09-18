a59b63590635a9c80b36cb1e894dac1f632526d2
ccannon062 <caleb.cristoph.cannon@gamil.com>
2025-09-18 17:39:09 -0500

    Fully integrated card component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-const Card = ({ cardName, value, trend, trendDirection, icon }) => {
-  //Trend direction will need to come into play later with the trend span display options.
+const Card = ({ cardName, value, trendDirection, icon }) => {
   return (
     <div className="bg-white p-10 rounded transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:shadow-xl">
       <div className="flex items-center gap-2">
         {icon}
         <h1 className="text-xl font-semibold text-slate-800">{cardName}</h1>
       </div>
-      <h2 className="text-slate-800 font-bold text-2xl pt-4">{value}</h2>
-      <div className={trend ? "text-green-700 pt-2" : "hidden"}>
-        {trend}
-        <span className="text-slate-600"> (24 h)</span>
-      </div>
+      <h2
+        className={
+          trendDirection
+            ? `${
+                trendDirection > 0 ? "text-green-500" : "text-red-500"
+              } font-bold text-2xl pt-4`
+            : "text-slate-800 font-bold text-2xl pt-4"
+        }
+      >
+        {value}
+      </h2>
     </div>
   );
 };
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,29 +2,34 @@ import React from "react";
 import { FaGlobeAmericas, FaBitcoin, FaEthereum } from "react-icons/fa";
 import Card from "../components/Card";
 import { useEffect, useState } from "react";
+import { PulseLoader } from "react-spinners";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+/* CREDIT FOR USED COMPONENTS
+
+  Credit to David Hu's React Spinner for the API loading animations
+  https://github.com/davidhu2000/react-spinners?tab=readme-ov-file 
+*/
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const Home = () => {
   const [marketCapPrice, setMarketCapPrice] = useState(0);
   const [tradingVolume, setTradingVolume] = useState(0);
   const [bitCoinDom, setBitcoinDom] = useState(0);
   const [ethDom, setEthDom] = useState(0);
+  const [top10Vol, set10Vol] = useState(0);
   const [btcPercentChange, setBtcPercentChange] = useState(0);
   const [ethPercentChange, setEthPercentChange] = useState(0);
-
-  //May need to find a different stat...
-  const [marketCapPercentChange, setMarketCapPercentChange] = useState(0);
 
   const cleanNumbers = (number) => {
     const num = Number(number);
     if (num >= 1e12) {
-      return (num / 1e12).toFixed(2) + "T";
+      return (num / 1e12).toFixed(2) + " T";
     } else if (num >= 1e9) {
-      return (num / 1e9).toFixed(2) + "B";
+      return (num / 1e9).toFixed(2) + " B";
     } else if (num >= 1e6) {
-      return (num / 1e6).toFixed(2) + "M";
+      return (num / 1e6).toFixed(2) + " M";
     } else {
       return num.toFixed(2).toString();
     }
@@ -41,7 +46,7 @@ const Home = () => {
       100
     );
   };
-
+  /*
   useEffect(() => {
     const fetchCoinData = async () => {
       try {
@@ -49,6 +54,8 @@ const Home = () => {
         let dayTradingVolume = 0;
         let btcMarketDominance = 0;
         let ethMarketDominance = 0;
+        let top10Volume = 0;
+
         const response = await fetch(`${BASE_URL}/assets?limit=200`, {
           headers: {
             Authorization: `Bearer ${API_KEY}`,
@@ -58,9 +65,11 @@ const Home = () => {
         coins.data.forEach((data) => {
           totalMarketCap += Number(data.marketCapUsd);
           dayTradingVolume += Number(data.volumeUsd24hr);
+          top10Volume += Number(data.volumeUsd24Hr);
         });
         setMarketCapPrice(totalMarketCap);
         setTradingVolume(dayTradingVolume);
+        set10Vol(top10Volume);
         const btc = coins.data.find((data) => data.id === "bitcoin");
         const eth = coins.data.find((data) => data.id === "ethereum");
         btcMarketDominance = (Number(btc.marketCapUsd) / totalMarketCap) * 100;
@@ -115,47 +124,50 @@ const Home = () => {
     };
     fetchCoinHist();
   }, []);
+*/
+  const loadSettings = <PulseLoader size={10} />;
 
   return (
-    <div className="min-h-screen bg-slate-200 w-full flex items-center justify-center px-3">
-      <div className="flex flex-col max-w-3xl gap-5 text-slate-600">
-        <h1 className="text-5xl font-bold text-slate-800 text-center mt-20 mb-10">
-          Global Market Snapshot
-        </h1>
-        <div className="flex flex-wrap gap-5 justify-center">
-          <Card
-            cardName="Total Market Cap (USD)"
-            value={
-              marketCapPrice == 0
-                ? "No data available"
-                : cleanNumbers(marketCapPrice)
-            }
-            icon={<FaGlobeAmericas className="text-slate-800 text-2xl" />}
-          />
-          <Card
-            cardName="24h Trading Volume"
-            value={
-              tradingVolume == 0
-                ? "No data available"
-                : cleanNumbers(tradingVolume)
-            }
-            icon={<FaGlobeAmericas className="text-slate-800 text-2xl" />}
-          />
-          <div className="flex gap-5 md:flex-col md:gap-2 items-center">
+    <>
+      <div className="min-h-screen bg-slate-200 w-full flex items-center justify-center px-5">
+        <div className="text-slate-600">
+          <h1 className="text-5xl font-bold text-slate-800 text-center mt-20 mb-20">
+            Global Market Snapshot
+          </h1>
+          <div className="grid md:grid-cols-2 gap-10">
+            <Card
+              cardName="Total Market Cap (USD)"
+              value={
+                marketCapPrice == 0
+                  ? loadSettings
+                  : cleanNumbers(marketCapPrice)
+              }
+              icon={<FaGlobeAmericas className="text-slate-800 text-2xl" />}
+            />
+            <Card
+              cardName="Coin Value Traded (24hr)"
+              value={tradingVolume == 0 ? loadSettings : cleanNumbers(top10Vol)}
+              icon={<FaGlobeAmericas className="text-slate-800 text-2xl" />}
+            />
             <Card
               cardName="BTC Dominance"
-              value={cleanNumbers(bitCoinDom) + "%"}
-              icon={<FaBitcoin />}
+              value={
+                bitCoinDom == 0 ? loadSettings : cleanNumbers(bitCoinDom) + " %"
+              }
+              icon={<FaBitcoin className="text-[#f7931a] text-2xl" />}
+              trendDirection={btcPercentChange}
             />
             <Card
               cardName="ETH Dominance"
-              value={cleanNumbers(ethDom) + "%"}
-              icon={<FaEthereum />}
+              value={ethDom == 0 ? loadSettings : cleanNumbers(ethDom) + " %"}
+              icon={<FaEthereum className="text-[#497493] text-2xl" />}
+              trendDirection={ethPercentChange}
             />
           </div>
         </div>
       </div>
-    </div>
+      <div className="min-h-screen bg-slate-300 w-full flex items-center justify-center px-5"></div>
+    </>
   );
 };
 
